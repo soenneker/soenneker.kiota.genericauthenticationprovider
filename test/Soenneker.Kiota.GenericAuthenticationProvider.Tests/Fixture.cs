f95191f89804f94c5b99f9d65f5c6a0383390245
Jake Soenneker <jake@soenneker.com>
2025-07-26 13:52:47 -0500

    fix tests

--- test/Soenneker.Kiota.GenericAuthenticationProvider.Tests/Fixture.cs
+++ test/Soenneker.Kiota.GenericAuthenticationProvider.Tests/Fixture.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Soenneker.Fixtures.Unit;
 using Soenneker.Utils.Test;
-using Soenneker.Kiota.GenericAuthenticationProvider.Registrars;
 
 namespace Soenneker.Kiota.GenericAuthenticationProvider.Tests;
 
@@ -25,7 +24,5 @@ public sealed class Fixture : UnitFixture
 
         IConfiguration config = TestUtil.BuildConfig();
         services.AddSingleton(config);
-
-        services.AddGenericAuthenticationProviderAsScoped();
     }
 }
